4bf27dcb16b5582bff9fa12750f8ad126e909a6a
Sunishka Sharma <sunishka2002@gmail.com>
2021-11-15 22:05:38 +0530

    values

---
+++ src/wrapperclass/MinMax.java
@@ -0,0 +1,15 @@
+package wrapperclass;
+
+public class MinMax {
+    public static void main(String[] args) {
+        System.out.println("Integer : \nMin Value = "+Integer.MIN_VALUE+"\nMax Value = "+Integer.MAX_VALUE);
+        System.out.println("Byte : \nMin Value = "+Byte.MIN_VALUE+"\nMax Value = "+Byte.MAX_VALUE);
+        System.out.println("Short : \nMin Value = "+Short.MIN_VALUE+"\nMax Value = "+Short.MAX_VALUE);
+        System.out.println("Float : \nMin Value = "+Float.MIN_VALUE+"\nMax Value = "+Float.MAX_VALUE);
+        System.out.println("Double : \nMin Value = "+Double.MIN_VALUE+"\nMax Value = "+Double.MAX_VALUE);
+        System.out.println("Long : \nMin Value = "+Long.MIN_VALUE+"\nMax Value = "+Long.MAX_VALUE);
+
+
+
+    }
+}
